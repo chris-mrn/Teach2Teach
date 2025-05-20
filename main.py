6fb72763c83791eb1ac4711cfb01fcc605e98c55
chris-mrn <109912652+chris-mrn@users.noreply.github.com>
2025-05-20 15:25:49 +0200

    plots function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from net.unet import Unet
 from models.Flow import GaussFlowMatching_OT
 from models.Score import NCSN
 import torchvision.transforms as transforms
-from utils import parse_arguments
+from utils import parse_arguments, show_images
 
 
 def main():
@@ -37,14 +37,19 @@ def main():
     net_fm = Unet()
     model_FM = GaussFlowMatching_OT(net_fm, device=args.device)
     optimizer_fm = torch.optim.Adam(net_fm.parameters(), 1e-2)
-    model_FM.train(optimizer_fm, dataloader1 , dataloader0 , n_epochs=1000)
+    model_FM.train(optimizer_fm, dataloader1 , dataloader0 , n_epochs=1)
     gen_FM_samples, hist_FM = model_FM.sample_from(X0[:10])
+
+    show_images(gen_FM_samples, title="Flow Matching Samples")
+
 
     net_score = Unet()
     model_score = NCSN(net_score, L=10, device=args.device)
     optimizer_score = torch.optim.Adam(net_score.parameters(), 1e-2)
-    model_score.train(optimizer_score, epochs=1000, dataloader=dataloader1, print_interval=10)
+    model_score.train(optimizer_score, epochs=1, dataloader=dataloader1, print_interval=10)
     gen_score_samples, hist_score = model_score.sample_from(X0[:10])
+
+    show_images(gen_score_samples, title="Score Matching Samples")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import argparse
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -13,3 +15,21 @@ def parse_arguments():
         help="Device to use",
     )
     return parser.parse_args()
+
+def show_images(tensor, title=None, nrow=5):
+    """
+    Plot a batch of images: tensor of shape (B, C, H, W)
+    """
+    # If tensor is on GPU, bring it to CPU
+    tensor = tensor.detach().cpu()
+
+    # Convert to grid of images
+    grid = vutils.make_grid(tensor, nrow=nrow, normalize=True)
+
+    # Convert to numpy and plot
+    plt.figure(figsize=(nrow * 2, 2.5))
+    plt.axis('off')
+    if title:
+        plt.title(title)
+    plt.imshow(grid.permute(1, 2, 0).numpy())
+    plt.show()
